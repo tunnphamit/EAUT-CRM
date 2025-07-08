e604e3532d4bd5fa5bbe35e87fb9eb03d8e72ba6
unknown <tuanpham24.it@gmail.com>
2025-07-08 11:22:15 +0700

    feat: update view

--- models/eaut_crm_program.py
+++ models/eaut_crm_program.py
@@ -5,14 +5,14 @@ class EautCrmProgram(models.Model):
     _description = 'Program' # Khóa
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    # Mã khóa (unique) (ví dụ: K21)
-    code = fields.Char(string='Program code', required=True, tracking=True)
+    # # Mã khóa (unique) (ví dụ: K21)
+    # code = fields.Char(string='Program code', required=True, tracking=True)
 
     # Tên Khóa (ví dụ: K11)
     name = fields.Char(string='Program name', required=True, tracking=True)
     
     # Năm khởi tạo
-    start_year = fields.Char(string='Start year', required=True, tracking=True)
+    start_year = fields.Char(string='Start year', tracking=True)
 
     # Quan hệ N:N: 1 chương trình nhiều sinh viên, 1 sinh viên nhiều chương trình
     student_ids = fields.Many2many(
@@ -25,6 +25,6 @@ class EautCrmProgram(models.Model):
     )
 
     # Ràng buộc Unique cho mã Khóa
-    _sql_constraints = [
-        ('program_code_unique', 'unique(code)', 'Program code must be unique!'),
-    ]
+    # _sql_constraints = [
+    #     ('program_code_unique', 'unique(code)', 'Program code must be unique!'),
+    # ]
